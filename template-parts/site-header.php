0a8989497a766f1489b5ef1278fd6dca60e13b7c
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-23 11:25:17 +0530

    home.php created for homepage

--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -19,7 +19,7 @@
         )); ?>
     </div>
 
-        <!-- Hamburger menu -->
+        <!-- For Hamburger menu -->
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
             <i class="fa fa-bars"></i>
         </a>
