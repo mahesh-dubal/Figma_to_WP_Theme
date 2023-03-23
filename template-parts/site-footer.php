069e17c86f70504212d8d10cd69dd61c25160d5c
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-23 15:55:01 +0530

    Revert "script code moved into main.js"
    This reverts commit d3f43af0111868a94ac2da56677d27ff4edac4e5.

--- template-parts/site-footer.php
+++ template-parts/site-footer.php
@@ -2,4 +2,17 @@
         <span id="footer-content">2023 &copy All Right Reserved</span>
     </div>
 
-   
+    <script>
+        function myFunction() {
+            var x = document.getElementById("myLinks");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }
+
+        const loadToolPage = () => {
+            return window.location.href = "#";
+        }
+    </script>
