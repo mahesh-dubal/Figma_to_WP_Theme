7152519cb0d5b3f9e9e8abd1c812b110c5d976f0
mahesh4320 <mahesh.dubal@wisdmlabs.com>
2023-03-23 14:46:46 +0530

    theme-support added for featured images

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 
 add_theme_support( 'custom-logo');
 add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
 
 function mrp_enqueue_styles() {
 
     wp_enqueue_style( 'custom-css', get_template_directory_uri() . '/assets/css/custom.css' );
 	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ) , '1.0', true );
 	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' );
-
 }
 add_action( 'wp_enqueue_scripts', 'mrp_enqueue_styles' );
 
@@ -17,5 +17,5 @@ add_action( 'wp_enqueue_scripts', 'mrp_enqueue_styles' );
 
 function custom_theme_setup() {
 	register_nav_menu('header-menu', __( 'Header Menu' ));
-  }
-  add_action( 'after_setup_theme', 'custom_theme_setup');
+}
+add_action( 'after_setup_theme', 'custom_theme_setup');
